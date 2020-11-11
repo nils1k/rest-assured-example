0d717d101c2b5f3138e0f1166e3c7dc4bdd36bb6
Kristian Nilsen <github@kristiannilsen.no>
2020-11-11 18:54:16 +0100

    Initial commit

---
+++ src/main/java/no/kristiannilsen/testrestexample/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package no.kristiannilsen.testrestexample.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
